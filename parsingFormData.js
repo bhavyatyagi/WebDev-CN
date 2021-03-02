a5cd62394d1b4098b1fb33dd7207f235ac4648e2
Bhavya Tyagi <55448429+bhavyatyagi@users.noreply.github.com>
2021-03-02 18:43:35 +0530

    Create parsingFormData.js

---
+++ parsingFormData.js
@@ -0,0 +1,65 @@
+const express=require('express');
+const port=786;
+const path=require('path'); //inbuilt node module doesnt require installation
+const app=express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+
+var contactList=[
+    {
+        name: "Arpan",
+        phone: "123456"
+    },
+    {
+        name: "Arpan1",
+        phone: "1234567"
+    },
+    {
+        name: "Arpan2",
+        phone: "12345678"
+    }
+]
+
+app.get('/',function(req,res)
+{
+    return res.render('home',{
+       title: "Contact List" ,
+       contact_list: contactList
+    });
+});
+app.get('/',function(request,response)
+{
+    return response.render('home',{title: 'Parser'});
+});
+
+app.post('/create-contact',function(request,response){
+    //WAY 1
+    // contactList.push({
+    //     name: request.body.name,
+    //     phone: request.body.phone
+    // })
+    // return response.redirect('/');
+
+    //ANOTHER WAY
+
+    contactList.push(request.body);
+    return response.redirect('back');
+});
+
+app.listen(port,function(error){
+    if(error)
+    {
+        console.log("Check app.listen, problem running server. ", error);
+    }
+    console.log("Server is up and running at port: ",port);
+}); 
+
+//steps
+// 1. install ejs
+// 2. App.set (view engine,view path)
+// 3. views directory +file
+// 4. render+response.render\
+// 5. home.ejs and not home.html
+// 6. check that home.ejs is in 'views' or mentioned folder
